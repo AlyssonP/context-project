42bd5a032e5f2c648e5ab4595828c5d6924566f7
AlyssonP <alysson.nascimento2111@gmail.com>
2024-03-15 00:39:00 -0300

    Realizando alguns ajustes

--- src/views/admin/user/TableUsers.jsx
+++ src/views/admin/user/TableUsers.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useUser } from "../../UserContext"
-import { Button, Container, Table } from "react-bootstrap"
+import { Button, Container} from "react-bootstrap"
 import DataTable from "react-data-table-component";
 
 const TableUsers = () => {
@@ -44,27 +44,6 @@ const TableUsers = () => {
     <Container>
       <h4>Users</h4>
       <DataTable columns={colums} data={users}/>
-
-      {/* <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, id) => {
-            return (
-              <tr key={id}>
-                <td>{id}</td>
-                <td>{user.nome}</td>
-                <td>{user.email}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </Table> */}
     </Container>
   )
 }
